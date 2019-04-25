7bad57a788bb4a75208f3c646562796afd17ec34
tysonlander <tysonlander@gmail.com>
2019-04-25 14:49:36 -0600

    JS3 finished code

--- javascript-3/student.js
+++ javascript-3/student.js
@@ -3,31 +3,53 @@
 
 //////////////////STEP 1////////////////////
 //Create an array called 'groceries' that contains three grocery items as a string
-
-// Code Here
+let groceries = ['hot coco', 'soda', 'vegetables'];
+// Code Here 
 
 //////////////////STEP 2////////////////////
 //Create a variable called 'yourName' that is equal to your name as a string
-
+let yourName = 'Tyson';
 // Code Here
 
 //////////////////STEP 3////////////////////
 //Create a function called 'setGroceryListTitle' that takes in one parameter called x. The function should return a string of 'XPARAM's Grocery List'.
-
+function setGroceryListTitle(x) {
+  return `${x} Grocery List`
+}
 // Code Here
 
 //////////////////STEP 4////////////////////
 //Create a function called 'addItem' that takes in one paramter called item. Add the item param to the end of the groceries array created above. Inside of addItem, invoke displayData() which is a function we created that takes care of some of the leg work unrelated to this module.
+function addItem(item){
+  groceries.push(item);
+  displayData();
+}
 
 // Code Here
 
 //////////////////STEP 5////////////////////
 //Create a function called removeItem that takes in one parmater called index. Inside of the function remove the item from the groceries array above using splice. Invoke the displayData() function again.
 
+function removeItem(index){
+  groceries.splice(index, 1)
+
+  displayData();
+}
 // Code Here
 
 //////////////////STEP 6////////////////////
-//Create a function called 'checkGroceryCount'. Inside of the function check the total number of groceries you need to get using .length. Using an else/if statement, check if the number is greater than or equal to 5, return the string 'That looks like a big trip' from the function. If the number of groceries is equal to one, return the string '1 item'. For numbers between 1 and 5, return the string 'NUMBER items' with number replacing the number of items
+//Create a function called 'checkGroceryCount'. Inside of the function check the total number of groceries you need to get using .length. Using an else/if statement, check if the number is greater than or equal to 5, return the string 'That looks like a big trip' -from the function. If the number of groceries is equal to one, return the string '1 item'. For numbers between 1 and 5, return the string 'NUMBER items' with number replacing the number of items
+
+function checkGroceryCount(){
+  let num = groceries.length;
+  if (num >= 5) {
+    return 'That looks like a big trip';
+  } else if (num === 1) {
+    return '1 item';
+  } else if (num < 5 && num > 1) {
+    return `${num} items`
+  }
+}
 
 // Code Here
 
