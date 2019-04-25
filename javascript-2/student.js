d7eac7844ce5958ead3cb20caaf51e0a51e6df40
tysonlander <tysonlander@gmail.com>
2019-04-25 14:19:29 -0600

    JS2 completed code

--- javascript-2/student.js
+++ javascript-2/student.js
@@ -1,28 +1,33 @@
 //////////////////SETUP////////////////////
 // Open javascripot-2/index.html in the browser. You should see "An Ode to Code" and a small box below it.
-
+ 
 //////////////////STEP 1////////////////////
 //Create a variable called lovesCode and set it true or false (as a boolean) depending on if you love code or not. It's okay if you don't, yet
-
+let lovesCode = true;
 // Code Here
 
 //////////////////STEP 2////////////////////
 //Create a variable called age that is your age as a number
-
+let age = 30;
 // Code Here
 
 //////////////////STEP 3////////////////////
 //Create a variable called name that is your name as a string
-
+let name = "tyson";
 // Code Here
 
 //////////////////STEP 4////////////////////
 //Create a variable called phrase that is initially set as an empty string
-
+let phrase = '';
 // Code Here
 
 //////////////////STEP 5////////////////////
 //Create an if statement that checkes to see if the lovesCode variable is equal to true, if it is, set the phrase variable created above to 'I love code', it it's not true set the phrase variable to 'I do not love to code'
+if(lovesCode === true) {
+  phrase = "I love code";
+} else {
+  phrase = "I do not love to code";
+}
 
 // Code Here
 
